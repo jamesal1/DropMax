81da0b7dd57040753c58790fde69796ceb11fc5c
James Austin Liu <jamesal1@arcus-2.ics.uci.edu>
2017-12-14 17:09:59 -0800

    also object oriented

--- customlayers.py
+++ customlayers.py
@@ -3,6 +3,7 @@ import keras.backend as K
 import tensorflow as tf
 from keras.layers import Input, Dense, Dropout, Flatten, Layer, Lambda, Multiply, Merge
 
+from sparsemax import sparsemax
 
 
 class SoftZ(Layer):
@@ -21,18 +22,35 @@ class SoftZ(Layer):
         unnoised = tf.sigmoid((1 / tau) * inputs)
         return K.in_train_phase(noised, unnoised, training=training)
 
-    def get_config(self):
-        config = {'stddev': self.stddev}
-        base_config = super(SoftZ, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+    # def get_config(self):
+    #     config = {'stddev': self.stddev}
+    #     base_config = super(SoftZ, self).get_config()
+    #     return dict(list(base_config.items()) + list(config.items()))
 
 
-class MaskSoftMax(Merge):
+# class MaskSoftMax(Merge):
+#
+#
+#     def merge_function(self, inputs):
+#         return None
+#         output = inputs[0]
+#         mask = inputs[1]
+#         raise ValueError(str(len(inputs)))
+#         return inputs[2]
+#         # e = K.exp(output - K.max(output, axis=1, keepdims=True)) * mask
+#         e = K.exp(output - K.max(output, axis=1, keepdims=True))
+#         # e = output * mask
+#         s = K.sum(e,axis=1,keepdims=True)
+#         return e/s
 
 
-    def _merge_function(self, inputs):
-        output = inputs[0]
-        mask = inputs[1]
-        e = K.exp(output - K.max(output, axis=1, keepdims=True)) * mask
-        s = K.sum(e,axis=1,keepdims=True)
-        return e/s
+
+def maskSoftMax(output,zee):
+    output = keras.layers.Lambda(lambda x: K.exp(x - K.max(x, axis=1, keepdims=True)))(output)
+    next_layer = Multiply()([output,zee])
+    next_layer = keras.layers.Lambda(lambda x: x+ 1e-4)(next_layer)
+    return keras.layers.Lambda(lambda x: x/K.sum(next_layer,axis=1,keepdims=True),name="softmax")(next_layer)
+
+
+def SparseMax():
+    return keras.layers.Lambda(lambda x: sparsemax(x))
